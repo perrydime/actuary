58624fc44ec21bf36f518c7fbb5965e35269abb7
root <root@ip-172-31-12-91.us-west-1.compute.internal>
2018-06-20 04:16:31 +0000

    Some work on stocks/portfolio.py

--- stocks/portfolio.py
+++ stocks/portfolio.py
@@ -1,17 +1,17 @@
 import numpy as np
 import csv
 
+print("\n")
 
 numStocks = int(raw_input('How many stocks do you want to use?  '))
 
 for j in range(numStocks):
 	exec("stockName" + str(j) + " = raw_input('What is the name of stock " + str(j) + "?  ')")
 
-
+print("\n")
 
 for i in range(numStocks):
-	exec("stock" + str(i) + " = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]\ncount = 0\nwith open('stock" + str(i) + ".csv', 'rb') as csvfile:\n\tread = csv.reader(csvfile, delimiter=',')\n\tfor row in read:\n\t\t#if(count > 0):\n\t\tstock" + str(i) + "[count] = float(row[1])\n\t\t#print row[0] + ',' + row[1]\n\t\tcount += 1\n\tcount = 0\nstock" + str(i) + "_returns = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]\nfor count in range(12):\n\tif(count < 11):\n\t\tstock" + str(i) + "_returns[count] = (stock" + str(i) + "[count+1] - stock" + str(i) + "[count])/stock" + str(i) + "[count]\nstock" + str(i) + "_cum_ret = 0.0\nfor count in range(11):\n\tstock" + str(i) + "_cum_ret += stock" + str(i) + "_returns[count]\nstock" + str(i) + "_avg_daily_ret = stock" + str(i) + "_cum_ret / 10.0\nstock" + str(i) + "_sum_squared_error = 0.0\nfor count in range(11):\n\tstock" + str(i) + "_sum_squared_error += ((stock" + str(i) + "_returns[count] - stock" + str(i) + "_avg_daily_ret)**2)\nstock" + str(i) + "_variance = stock" + str(i) + "_sum_squared_error/10.0\nstock" + str(i) + "_standard_deviation = np.sqrt(stock" + str(i) + "_variance)\nprint('stock" + str(i) + "')\nprint('Return = ' + str(stock" + str(i) + "_cum_ret))\nprint('Variance = ' + str(stock" + str(i) + "_variance))\nprint('Standard Deviation = ' + str(stock" + str(i) + "_standard_deviation))")
-
+	exec("stock" + str(i) + " = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]\ncount = 0\nwith open('stock" + str(i) + ".csv', 'rb') as csvfile:\n\tread = csv.reader(csvfile, delimiter=',')\n\tfor row in read:\n\t\t#if(count > 0):\n\t\tstock" + str(i) + "[count] = float(row[1])\n\t\t#print row[0] + ',' + row[1]\n\t\tcount += 1\n\tcount = 0\nstock" + str(i) + "_returns = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]\nfor count in range(12):\n\tif(count < 11):\n\t\tstock" + str(i) + "_returns[count] = (stock" + str(i) + "[count+1] - stock" + str(i) + "[count])/stock" + str(i) + "[count]\nstock" + str(i) + "_cum_ret = 0.0\nfor count in range(11):\n\tstock" + str(i) + "_cum_ret += stock" + str(i) + "_returns[count]\nstock" + str(i) + "_avg_daily_ret = stock" + str(i) + "_cum_ret / 10.0\nstock" + str(i) + "_sum_squared_error = 0.0\nfor count in range(11):\n\tstock" + str(i) + "_sum_squared_error += ((stock" + str(i) + "_returns[count] - stock" + str(i) + "_avg_daily_ret)**2)\nstock" + str(i) + "_variance = stock" + str(i) + "_sum_squared_error/10.0\nstock" + str(i) + "_standard_deviation = np.sqrt(stock" + str(i) + "_variance)")
 
 
 
@@ -55,4 +55,4 @@ for i in range(numStocks):
 print('Portfolio Return = ' + str(port_cum_ret/numStocks))
 
 for i in range(numStocks):
-	exec("print(stockName" + str(i) + ")\nprint('Return = ' + str(stock" + str(i) + "_cum_ret))\nprint('Weight = ' + str((finalWeight" + str(i) + " / totalFinalWeight)*100) + '  %')")
+	exec("print(stockName" + str(i) + ")\nprint('Return = ' + str(stock" + str(i) + "_cum_ret))\nprint('Standard Deviation = ' + str(stock" + str(i) + "_standard_deviation))\nprint('Weight = ' + str((finalWeight" + str(i) + " / totalFinalWeight)*100) + '  %')")
